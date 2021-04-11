fd2c335dfdcedf9996fd34ebcddf779de19cf841
jarvispujol <superpujol@gmail.com>
2021-04-11 16:39:55 -0300

    adding backend files

---
+++ backend/src/main/java/com/jpdev/jpcash/JpcashApplication.java
@@ -0,0 +1,13 @@
+package com.jpdev.jpcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpcashApplication.class, args);
+	}
+
+}
